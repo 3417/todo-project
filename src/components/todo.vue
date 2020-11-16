fb30fd069ea21d207ff8bdc0d0de7fa25f60904c
1789719643@qq.com <1789719643@qq.com>
2020-11-16 16:40:53 +0800

    :art: update

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -36,14 +36,15 @@
             <div class="todo__value" :class="{ 'is-ok': it.complete }">
               {{ it.text }}
             </div>
-            <i class="todo-del cursor" @click="todoDelSingle">X</i>
+            <i class="todo-del cursor" @click="todoDelSingle(it)">X</i>
           </li>
         </ul>
       </section>
       <!-- 操作栏 -->
       <section class="todo-tool">
         <div class="items cursor">
-          <strong>{{ 999 }}</strong> {{ 99 > 1 ? "items" : "item" }} left
+          <strong>{{ showAitems }}</strong>
+          {{ showAitems > 1 ? "items" : "item" }} left
         </div>
         <div class="bar">
           <p class="cursor" @click="todoAll">All</p>
@@ -57,7 +58,7 @@
 </template>
 
 <script>
-import { reactive, ref, toRefs } from "vue";
+import { reactive, ref, toRefs, computed, onMounted } from "vue";
 import useStorage from "../utils/storage";
 export default {
   comments: true,
@@ -66,56 +67,69 @@ export default {
       msg: "TODOS",
       value: "",
       panel: true,
-      list: [
-        {
-          text: "这是待完成的事项1",
-          complete: false,
-        },
-        {
-          text: "这是待完成的事项2",
-          complete: false,
-        },
-        {
-          text: "这是待完成的事项3",
-          complete: true,
-        },
-      ],
+      list: [],
+      jsonkey: "TODO-LIST",
     });
     const todoChange = function (item) {
-      console.log("change==>", item.text, item.complete);
+      console.log("change==>", item.id, item.complete);
+      const local = useStorage.getItem(state.jsonkey);
+      let newLoca = local.map((el) => (el.id === item.id ? item : el));
+      useStorage.setItem(state.jsonkey, newLoca);
     };
     // 控制显示隐藏
     const RotateItem = function () {
       state.panel = !state.panel;
     };
 
+    // 获取所有的条目源数据
+    const getAllCount = function () {
+      return useStorage.getItem(state.jsonkey);
+    };
+
     const addTodo = function () {
       const text = state.value.trim();
       state.list.unshift({
         text,
+        id: new Date().valueOf(),
         complete: false,
       });
       state.value = "";
+      useStorage.setItem(state.jsonkey, state.list);
     };
 
     const todoAll = function () {
       console.log("切换到全部");
+      state.list = getAllCount();
     };
+    // 切换显示不同的items
+    const showAitems = computed(() => {
+      return state.list.length;
+    });
 
     const todoActive = function () {
       console.log("切换到正在进行中的");
-      // state.list = state.list.filter(el=>!el.complete)
+      state.list = getAllCount().filter((el) => !el.complete);
     };
     const todoComplete = function () {
       console.log("切换到已经完成的");
-      // state.list = state.list.filter(el=>el.complete)
+      state.list = getAllCount().filter((el) => el.complete);
     };
     const todoClearCompl = function () {
       console.log("清除已经完成的todo");
     };
-    const todoDelSingle = function(){
-      console.log('删除单个的todo')
-    }
+    const todoDelSingle = function (item) {
+      console.log("删除单个的todo",item.id);
+      let del_id = state.list.findIndex(el=>el.id === item.id);
+      let del_local = getAllCount().findIndex(ele=>ele.id === item.id);
+      let del_data = JSON.parse(JSON.stringify(getAllCount()));
+      del_data.splice(del_local,1);
+      state.list.splice(del_id,1);
+      useStorage.setItem(state.jsonkey,del_data );
+    };
+    onMounted(() => {
+      const sourceDate = useStorage.getItem(state.jsonkey);
+      state.list = sourceDate;
+    });
     return {
       todoChange,
       RotateItem,
@@ -125,7 +139,8 @@ export default {
       todoActive,
       todoComplete,
       todoClearCompl,
-      todoDelSingle
+      todoDelSingle,
+      showAitems,
     };
   },
 };
